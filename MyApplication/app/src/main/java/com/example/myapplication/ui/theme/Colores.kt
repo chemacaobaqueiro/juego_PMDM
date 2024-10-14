378de666e01861d052a5d59611f230c46580b562
Chema <jcaobaqueiro@danielcastelao.org>
2024-10-14 12:32:08 +0200

    Necesita cambios gráficos

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/theme/Colores.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+enum class Colores (val valorcolor:Int,val color:Color){
+    Rojo(1, Color.Red),
+    Verde(2, Color.Green),
+    Azul(3, Color.Blue),
+    Amarillo(4, Color.Yellow)
+}
