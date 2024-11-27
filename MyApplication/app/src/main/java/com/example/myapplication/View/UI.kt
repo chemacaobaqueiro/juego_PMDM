76819ad5e6a5a2586a33e671ca79a36857bdee35
Chema <jcaobaqueiro@danielcastelao.org>
2024-11-27 18:05:12 +0100

    Pequeños cambios

--- MyApplication/app/src/main/java/com/example/myapplication/View/UI.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/View/UI.kt
@@ -1,98 +1,190 @@
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.Datos.Colores
+import com.example.myapplication.ViewModel.ViewModel
+
+
 
 @Composable
-fun botones(coloresBotones: MutableList<Int>, listaColor: List<Color>, valoresColores: List<Int>) {
-    // Función interna para crear botones con colores y lógica asociados
-    @Composable
-    fun crearBoton(color: Color, colorValor: Int) {
-        Button(
-            onClick = { coloresBotones.add(colorValor) },
-            colors = ButtonDefaults.buttonColors(containerColor = color),
+fun showText(text: String, fontSize: Int, fontWeight: FontWeight, modifier: Modifier) {
+    Text(
+        text = text,
+        fontSize = fontSize.sp,
+        fontWeight = fontWeight,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun showAciertos(aciertos: Int) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(top = 90.dp, start = 126.dp)
+    ) {
+        showText(
+            text = "Aciertos: $aciertos",
+            fontSize = 25,
+            fontWeight = FontWeight.Bold,
             modifier = Modifier
-                .clip(CircleShape)
-                .padding(3.dp)
-                .size(95.dp)
-        ) {
-            // Contenido vacío porque el botón solo necesita representar color
-        }
+        )
+        Log.d("Record", aciertos.toString())
+    }
+}
+
+@Composable
+fun RecordMaximo(record: Int) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(top = 50.dp, start = 130.dp)
+    ) {
+        showText(
+            text = "Record: $record",
+            fontSize = 25,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+        )
+    }
+}
+
+@Composable
+fun buttonColor(
+    viewModel: MyViewModel,
+    listaColores: MutableList<Int>,
+    colorValor: Int,
+    color: Color
+) {
+    val _activo by remember {
+        mutableStateOf(viewModel.estadoLiveData.value?.botonesColoresActivos ?: true)
     }
 
-    // Itera sobre la lista de colores y valores, creando botones
-    listaColor.zip(valoresColores).forEach { (color, valor) ->
-        crearBoton(color, valor)
+    viewModel.estadoLiveData.observe(LocalLifecycleOwner.current) {
+        // Actualiza el estado local según el LiveData
+    }
+
+    Button(
+        enabled = _activo,
+        onClick = { viewModel.addColor(colorValor, listaColores, viewModel.getRandom()) },
+        colors = ButtonDefaults.buttonColors(containerColor = color),
+        modifier = Modifier
+            .clip(CircleShape)
+            .padding(3.dp)
+            .size(95.dp)
+    ) { /* Botón vacío */ }
+}
+
+@Composable
+fun showRondas(numeroRondas: Int) {
+    showText(
+        text = "Ronda: $numeroRondas",
+        fontSize = 20,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(top = 100.dp)
+    )
+}
+
+@Composable
+fun showButtonStart(viewModel: MyViewModel) {
+    val _activo by remember {
+        mutableStateOf(viewModel.estadoLiveData.value?.startActivo ?: true)
+    }
+
+    val rosa = Color(0xFFFF00C9)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(top = 40.dp)
+    ) {
+        Button(
+            enabled = _activo,
+            onClick = { viewModel.setRandom() },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = rosa,
+                contentColor = Color.Black
+            ),
+            modifier = Modifier
+                .size(145.dp)
+                .clip(RectangleShape)
+        ) {
+            showText(
+                text = "Start",
+                fontSize = 25,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun gameButtons(viewModel: MyViewModel, listaColores: MutableList<Int>) {
+    val colorRojo by viewModel.colorRojoLiveData.observeAsState(viewModel.getColorRed())
+    val colorVerde by viewModel.colorVerdeLiveData.observeAsState(viewModel.getColorGreen())
+    val colorAzul by viewModel.colorAzulLiveData.observeAsState(viewModel.getColorBlue())
+    val colorAmarillo by viewModel.colorAmarilloLiveData.observeAsState(viewModel.getColorYellow())
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(70.dp).padding(top = 190.dp, start = 15.dp)
+    ) {
+        Row {
+            buttonColor(viewModel, listaColores, Colores.ROJO.valorColor, colorRojo)
+            buttonColor(viewModel, listaColores, Colores.VERDE.valorColor, colorVerde)
+        }
+        Row {
+            buttonColor(viewModel, listaColores, Colores.AZUL.valorColor, colorAzul)
+            buttonColor(viewModel, listaColores, Colores.AMARILLO.valorColor, colorAmarillo)
+        }
     }
 }
 
 @Composable
 fun myApp(viewModel: MyViewModel) {
-    val listaColores = remember { mutableStateListOf<Int>() } // Lista de colores seleccionados
-    val isStartButtonPressed = remember { mutableStateOf(true) } // Control de inicio del juego
-    val presioneStart = remember { mutableStateOf(false) } // Control para saber si se presionó el botón de inicio
+    val record by viewModel.recordLiveData.observeAsState(viewModel.getRecord())
+    val rondas by viewModel.rondasLiveData.observeAsState(viewModel.getRondas())
+    val aciertos by viewModel.aciertosLiveData.observeAsState(viewModel.getAciertos())
 
-    val listaColor1 = listOf(Color.Red, Color.Green) // Colores para el primer grupo
-    val valoresColores1 = listOf(Colores.ROJO.valorColor, Colores.VERDE.valorColor)
-
-    val listaColor2 = listOf(Color.Blue, Color.Yellow) // Colores para el segundo grupo
-    val valoresColores2 = listOf(Colores.AZUL.valorColor, Colores.AMARILLO.valorColor)
+    val listaColores = remember { mutableStateListOf<Int>() }
 
     Box(modifier = Modifier.fillMaxSize()) {
-        // Muestra récord, texto inicial y otros elementos
+        // Fondo de la app
+        val backgroundImage = painterResource(id = R.drawable.fondo)
+        Image(
+            painter = backgroundImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        // Mostrar récord y aciertos
         Column {
-            RecordMaximo(viewModel.getMaxRecord())
-            initialText(viewModel.getSaludoInicio())
-            showRecord(viewModel.getRecord())
+            RecordMaximo(record)
+            showAciertos(aciertos)
         }
 
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(70.dp)
-                .padding(top = 190.dp, start = 15.dp)
-        ) {
-            Row {
-                // Llama a la función `botones` para el primer grupo de botones
-                botones(listaColores, listaColor1, valoresColores1)
-            }
-            Row {
-                // Llama a la función `botones` para el segundo grupo de botones
-                botones(listaColores, listaColor2, valoresColores2)
-            }
-
-            // Muestra la ronda actual
-            showRondas(viewModel.getRondas())
-
-            // Botón para iniciar el juego
-            startGame(isStartButtonPressed, presioneStart1 = presioneStart, viewModel = viewModel)
-
-            // Comprueba si la lista tiene el tamaño esperado para procesar el juego
-            if (listaColores.size == 3) {
-                game(viewModel, listaColores)
-            }
-        }
-    }
-}
-
-/**
- * Muestra el número de rondas actual.
- */
-@Composable
-fun showRondas(ronda: Int) {
-    Text(
-        text = "Ronda: $ronda",
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        modifier = Modifier
-            .padding(top = 100.dp)
-    )
-}
-
-/**
- * Vista previa de la aplicación con tema.
- */
-@Preview(showBackground = true)
-@Composable
-fun ColorButtonsPreview() {
-    MyApplicationTheme {
-        myApp(viewModel = MyViewModel())
+        // Botones y rondas
+        gameButtons(viewModel, listaColores)
+        showRondas(rondas)
+        showButtonStart(viewModel)
     }
 }
